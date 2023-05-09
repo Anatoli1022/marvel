7b5c67fc602cf74af31035bd57b9fc1b6eb8b602
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-05-10 01:40:45 +0300

    feat: adds mobile version

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -10,9 +10,9 @@ const AppHeader = () => {
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="#">Characters</a></li>
+                    <li><a className='app__link' href="#">Characters</a></li>
                     /
-                    <li><a href="#">Comics</a></li>
+                    <li><a className='app__link' href="#">Comics</a></li>
                 </ul>
             </nav>
         </header>
